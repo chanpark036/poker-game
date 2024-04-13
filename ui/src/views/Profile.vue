488b74d3bcbdc046a35a3cac8a26456c6350fd3a
chp25 <chp25@duke.edu>
2024-04-13 15:40:16 -0400

    commented out delete part

--- ui/src/views/Profile.vue
+++ ui/src/views/Profile.vue
@@ -7,7 +7,9 @@
                     <h5>Your PlayerId: {{ user.preferred_username }}</h5>
                     <b-button :pressed.sync="editMode" variant="primary" style="margin-left:auto" class="mb-2">Edit Mode</b-button>
                 </b-card-text>
-                
+                <b-card-text v-if="profilePic">
+                    <img :src="'data:image/png;base64,' + profilePic" width="100" height="100">
+                </b-card-text>
 
                 <b-card-text v-if="playerInfo && !editMode">
                     <p>Name: {{ playerInfo.name }}</p>
@@ -15,7 +17,7 @@
                     <p>Amount: {{ playerInfo.earnings }}</p>
                 </b-card-text>
                 <b-card-text v-else>
-                    <b-form @submit="save">
+                    <b-form>
                         <b-form-group id="input-group-1" label="Your Name:" label-for="input-1" description="First and Last">
                             <b-form-input id="input-1" v-model="form.name" placeholder="Enter name" required></b-form-input>
                         </b-form-group>
@@ -28,11 +30,14 @@
                             <b-form-input id="input-3" v-model="form.earnings" placeholder="Amount in Account" required></b-form-input>
                         </b-form-group>
 
-                        <b-form-file v-model="file" class="mt-3" plain accept="image/jpeg, image/png" name="file"></b-form-file>
+                        <b-form-file v-model="file" class="mt-3" plain accept="image/png" name="file"></b-form-file>
                         <div class="mt-3">Selected file: {{ file ? file.name : '' }}</div>
 
-                        <b-button type="submit" variant="primary">Submit</b-button>
+                        <!-- <b-button type="submit" variant="primary">Submit</b-button> -->
+                        <b-button @click="save">submit</b-button>
                         <b-button @click="saveImage" variant="primary">save image</b-button>
+                        <!-- <b-button @click="getImage" variant="primary">get image</b-button> -->
+                        
                     </b-form>
                 </b-card-text>
 
@@ -42,7 +47,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, inject, Ref, onMounted } from 'vue'
+import { watch, ref, inject, Ref, onMounted} from 'vue'
 import { Player } from '../../../server/game/model'
 import {PlayerProfileInfo} from '../../../server/site/data'
 
@@ -58,6 +63,7 @@ const form: Ref<PlayerProfileInfo> = ref({
 })
 const editMode = ref(false)
 const file: Ref<File | null> = ref(null)
+const profilePic = ref('')
 
 //if they already have player profile-> display player profile + access to everything else
 //if they do not have player profile -> they MUST create player profile first before they can do anything else
@@ -79,14 +85,17 @@ async function refresh() {
                 form.value.profilePic = playerInfo.value?.profilePic || ''
                 form.value.gamesPlayed = playerInfo.value?.gamesPlayed || 0
                 console.log(playerInfo.value)
+                await getImage()
             }
         } catch (error) {
             console.error(error);
             alert('An error occurred while fetching player data.');
         }
     }
+    editMode.value=false
 }
 onMounted(refresh)
+// watch(user, refresh, { immediate: true })
 
 async function save() {
     console.log(form)
@@ -96,25 +105,56 @@ async function save() {
         headers: { "Content-Type": "application/json" }, 
         body: JSON.stringify(form.value)}
     )
-    await refresh()
+    await saveImage()
+    console.log("done saving")
+    // await refresh()
 }
 
 async function saveImage() {
     console.log(file.value)
-    if(file.value != null)
+    if(user.value && file.value != null)
     {
-        const formData = new FormData()
-        formData.append('file',file.value)
-        formData.forEach((value, key) => {
-        console.log("key %s: value %s", key, value);
-        })
-        
-        await fetch(
-            "/api/profileInfo/upload",
-            { method: "POST",
-            body: formData}
-        )
+        if(file.value.size > 3000000){
+            throw new Error('File too large try again');
+        }
+        else{
+            const newFile = new File([file.value], user.value.preferred_username+'.png', {type: file.value.type});
+            const formData = new FormData()
+            formData.append('file',newFile)
+            formData.forEach((value, key) => {
+            console.log("key %s: value %s", key, value);
+            })
+            console.log(profilePic.value)
+            try{
+                if(profilePic.value){
+                    await fetch("/api/profileInfo/deletePicture",{ method: "DELETE" , body: formData})          
+                }
+            }
+            catch(e){
+                console.log('No image found')
+            }
+            finally{
+                console.log('uploading Picture')
+                await fetch(
+                "/api/profileInfo/uploadPicture",
+                { method: "POST",
+                body: formData})
+            }
+        }
     }
-
 }
+
+async function getImage() {
+    if(user.value){
+        const profilePicDataResponse = await fetch("/api/profileInfo/getPicture")
+        const profilePicData = await profilePicDataResponse.json()
+        console.log(profilePicData)
+        console.log(profilePicDataResponse.status)
+        if(profilePicDataResponse.status === 200)
+        {
+            profilePic.value = profilePicData.body
+        }
+    }
+}
+
 </script>
