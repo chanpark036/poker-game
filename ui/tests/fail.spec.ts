8e85f11ae22e96c1bfb045425a3569ca24a24923
Connie Vi <cv99@duke.edu>
2024-04-19 18:58:11 -0400

    deleted failing e2e test

--- ui/tests/fail.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('this test should fail', async ({ page }) => {
-    await page.goto('http://127.0.0.1:31000/api/login?key=will&user=fail&role=player');
-    await expect(page.getByRole('link', { name: 'None' })).toBeVisible();
-});
